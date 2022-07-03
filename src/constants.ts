b2318a0117776c36d053c0c710bb8f8f805618c9
Paulo Cabral Sanz <paulosanz@poli.ufrj.br>
2022-07-02 21:44:44 -0300

    Update host

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export default {
   API_HOST: process.env.NODE_ENV === 'production'
-    ? 'https://iop-monitor-server.tk:4001'
+    ? 'https://api.internet-of-plants.org:4001'
     : 'http://localhost:4001',
 };
