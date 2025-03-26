5855f3d72780db65874dc74f56055d05425fab2d
Paulo Cabral Sanz <paulo@railway.app>
2025-03-26 00:19:58 -0300

    Update backend url to be railway

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export default {
-  API_HOST: process.env.BACKEND_URL ?? process.env.NODE_ENV === 'production'
-      ? 'https://api.internet-of-plants.org:4001'
+  API_HOST: process.env.NODE_ENV === 'production'
+      ? 'https://back.internet-of-plants.org'
       : 'http://localhost:4001'
 }
